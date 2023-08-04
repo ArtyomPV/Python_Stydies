78e5e0715284b230be596dd41e2abdd30841ce42
Artyom <itcoding76@gmail.com>
2023-08-04 08:38:03 +0200

    настройка flatpages

--- SkillFactory/fastfood_D2/project/project/settings.py
+++ SkillFactory/fastfood_D2/project/project/settings.py
@@ -52,6 +52,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware',
 ]
 
 ROOT_URLCONF = 'project.urls'
