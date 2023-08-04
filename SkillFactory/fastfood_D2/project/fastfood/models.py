9ce876f775febdccefc3e5b3059ec81cb6463225
Artyom <itcoding76@gmail.com>
2023-08-04 21:01:15 +0200

    добавлен ProductOrder

--- SkillFactory/fastfood_D2/project/fastfood/models.py
+++ SkillFactory/fastfood_D2/project/fastfood/models.py
@@ -73,3 +73,26 @@ class Staff(models.Model):
     class Meta:
         verbose_name = 'Staff'
         verbose_name_plural = 'Staff'
+
+
+class ProductOrder(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    _amount = models.SmallIntegerField(default=1)
+
+    def __str__(self):
+        return f'Order #{self.order.pk} - Product: {self.product.name} x {self.amount}'
+
+    def product_sum(self):
+        product_price = self.product.price
+        return product_price * self.amount
+
+    @property
+    def amount(self):
+        return self._amount
+
+    @amount.setter
+    def amount(self, value):
+        self._amount = int(value) if value >= 0 else 0
+        self.save()
+
