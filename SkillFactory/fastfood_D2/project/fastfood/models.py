bec105323a48aa26daff5207133cfa5929ff477a
Artyom <itcoding76@gmail.com>
2023-08-04 20:56:27 +0200

    добавлен class Order

--- SkillFactory/fastfood_D2/project/fastfood/models.py
+++ SkillFactory/fastfood_D2/project/fastfood/models.py
@@ -1,3 +1,28 @@
+from datetime import datetime
+
 from django.db import models
 
-# Create your models here.
+
+class Order(models.Model):
+    time_in = models.DateTimeField(auto_now_add=True)
+    time_out = models.DateTimeField(auto_now=True)
+    cost = models.IntegerField(default=0)
+    staff = models.ForeignKey('Staff', on_delete=models.CASCADE)
+    take_away = models.BooleanField(default=False)
+    complete = models.BooleanField(default=False)
+    product = models.ManyToManyField('Product', through='ProductOrder')
+
+    def __str__(self):
+        return f'Order #{self.pk} - Total: {self.cost}'
+
+    @property
+    def total(self):
+        sum = self.product.aggregate(sum=models.Sum('price'))['sum']
+        self.cost = sum
+        self.save()
+        return sum
+
+    def finish_order(self):
+        self.time_out = datetime.now()
+        self.complete = True
+        self.save()
