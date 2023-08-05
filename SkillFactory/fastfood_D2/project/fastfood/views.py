12b425a7600ed3fedaf05bf7926e8ef4232bac16
Artyom <itcoding76@gmail.com>
2023-08-05 09:52:10 +0200

    создание представления и html страницы для просмотра одного Product по id

--- SkillFactory/fastfood_D2/project/fastfood/views.py
+++ SkillFactory/fastfood_D2/project/fastfood/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Product
 
 
@@ -10,4 +10,10 @@ class ProductsList(ListView):
     # выводиться наши объекты
     context_object_name = 'products'  # имя списка в котором лежат все объекты,
     # его нужно указать, чтобы обратиться к самому списку через HTML шаблон
+    queryset = Product.objects.order_by('-id')
 
+
+class ProductDetail(DetailView):
+    model = Product
+    template_name = 'fastfood/product.html'
+    context_object_name = 'product'
--- SkillFactory/fastfood_D2/project/project/urls.py
+++ SkillFactory/fastfood_D2/project/project/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('pages/', include('django.contrib.flatpages.urls')),
     path('products/', include('fastfood.urls')),
+    path('product/', include('fastfood.urls')),
 ]
