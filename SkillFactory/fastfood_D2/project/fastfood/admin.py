21ca5c2ad60bc4ff6e71c19244d6d16ef7ce33f3
Artyom <itcoding76@gmail.com>
2023-08-05 07:42:12 +0200

    регистрация моделей в панели администратора

--- SkillFactory/fastfood_D2/project/fastfood/admin.py
+++ SkillFactory/fastfood_D2/project/fastfood/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Product, Order, Staff, ProductOrder
 
-# Register your models here.
+admin.site.register(Product)
+admin.site.register(Order)
+admin.site.register(Staff)
+admin.site.register(ProductOrder)
+
