6a0d46a0117228e8fb440a2ce9667e787673f3b3
Artyom <itcoding76@gmail.com>
2023-08-05 08:39:29 +0200

    создание представления и html страницы для Products

--- SkillFactory/fastfood_D2/project/fastfood/views.py
+++ SkillFactory/fastfood_D2/project/fastfood/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Product
 
-# Create your views here.
+
+class ProductsList(ListView):
+    model = Product  # модель, объекты которой необходимо выводить
+    template_name = 'fastfood/products.html'  # указывает имя шаблона,
+    # в котором будет лежать HTML, в нем будет инструкция о том как будут
+    # выводиться наши объекты
+    context_object_name = 'products'  # имя списка в котором лежат все объекты,
+    # его нужно указать, чтобы обратиться к самому списку через HTML шаблон
+
