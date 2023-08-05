e771c7e227593d8e83cd7a1bf2abe024db88d509
Artyom <itcoding76@gmail.com>
2023-08-05 16:35:47 +0200

    added class Author

--- SkillFactory/NewsPaper_D2/project/NewsPaperApp/models.py
+++ SkillFactory/NewsPaper_D2/project/NewsPaperApp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import User
+from django.db import models
+from django.db.models import Sum
+
+
+class Author(models.Model):
+    author_name = models.OneToOneField(User, on_delete=models.CASCADE)
+    rating = models.IntegerField(default=0)
+
+    def update_rating(self):
+        postRat = self.post_set.all().aggregate(posting=Sum('rating_post'))
+        prat = 0
+        prat += postRat.get('posting')
+        self.rating = prat
+        self.save()
+
+
